cb55b7ac2fc66674d919adceaae3f5d683e4d332
HR-2019 <56949560+HR-2019@users.noreply.github.com>
2022-10-25 20:27:14 -0500

    Actualización del Consumer

--- src/main/java/com/example/backresultados/service/impl/ConsumerServiceImpl.java
+++ src/main/java/com/example/backresultados/service/impl/ConsumerServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.backresultados.service.ConsumerService;
 import com.example.backresultados.service.ResultadoService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.AmqpIOException;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -24,23 +26,48 @@ public class ConsumerServiceImpl implements ConsumerService {
     @Override
     @RabbitListener(
             bindings = @QueueBinding(
-                    value = @Queue(value = "${spring.rabbitmq.queue}",durable = "true"),
+                    value = @Queue(value = "${spring.rabbitmq.queue}", durable = "true"),
                     exchange = @Exchange(value = "${spring.rabbitmq.exchange}"),
                     key = "${spring.rabbitmq.routingkey}")
     )
+
     public Object consumerMessage(String objId) throws AmqpIOException {
+
         System.out.println("=============== Message ==================");
         System.out.println(objId);
         System.out.println("==========================================");
-        Resultado obj= resultadoService.getResultado(UUID.fromString(objId));
-        if(obj==null){
-            return null;
-        }
-        else{
-            ObjectMapper objmap = new ObjectMapper();
+
+        UUID resultadoId;
+
+        try {
+            resultadoId = UUID.fromString(objId);
+        } catch (Exception e) {
+            ObjectMapper obj = new ObjectMapper();
             try {
-                return objmap.writeValueAsString(obj);
-            }catch(JsonProcessingException e){
+                return obj.writeValueAsString("Error: El id del resultado no es un UUID válido");
+            } catch (JsonProcessingException ex) {
+                return null;
+            }
+        }
+
+        Resultado product = resultadoService.getResultado(resultadoId);
+
+        if (product == null) {
+            System.out.println("No existe un resultado con ese ID");
+            return null;
+        } else {
+
+            System.out.println("Existe el resultado");
+            System.out.println(product.toString());
+
+            ObjectMapper obj = JsonMapper.builder()
+                    .addModule(new JavaTimeModule())
+                    .build();
+
+            try {
+                return obj.writeValueAsString(product).replaceAll("^\\{\\Xid\\X:\\d+,", "{"); //test
+            } catch (JsonProcessingException e) {
+                System.out.println(e.toString());
                 return null;
             }
         }
